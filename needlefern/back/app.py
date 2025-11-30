ab7329ae4e15bc523f2e554e00e4833a6a498165
FreyllaR <olkov.kir1@yandex.ru>
2025-11-30 18:53:11 +0100

    add directories and clean code

--- needlefern/back/app.py
+++ needlefern/back/app.py
@@ -138,4 +138,4 @@ async def submit_order(order: OrderData):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="192.168.1.9", port=8000, reload=True)
